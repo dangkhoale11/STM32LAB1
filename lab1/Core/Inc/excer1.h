8ac37b8054a72f7d715e0fcf8581519825f21e99
khoale11-admin <khoa.le11@hcmut.edu.vn>
2024-09-26 12:53:18 +0700

    commit ex1

---
+++ lab1/Core/Inc/excer1.h
@@ -0,0 +1,21 @@
+/*
+ * excer1.h
+ *
+ *  Created on: Sep 26, 2024
+ *      Author: ADMIN
+ */
+
+#ifndef INC_EXCER1_H_
+#define INC_EXCER1_H_
+
+
+extern int count;
+
+void setCnt(int cnt);
+
+void excer1(GPIO_InitTypeDef *, GPIO_InitTypeDef *, uint16_t, uint16_t  );
+
+#endif /* INC_EXCERSICE1_H_ */
+
+
+
