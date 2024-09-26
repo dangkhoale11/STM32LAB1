738cd11482d748433659b2105ede27f28b4afb2b
khoale11-admin <khoa.le11@hcmut.edu.vn>
2024-09-26 14:33:07 +0700

    UPDATE EX4

--- lab1/Core/Src/main.c
+++ lab1/Core/Src/main.c
@@ -181,12 +181,32 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, SEG_0_Pin|SEG_1_Pin|SEG_2_Pin|SEG_3_Pin
+                          |SEG_4_Pin|SEG_5_Pin|SEG_6_Pin|SEG_7_Pin
+                          |SEG_8_Pin|SEG_9_Pin|SEG_10_Pin|SEG_11_Pin
+                          |SEG_12_Pin|SEG_13_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_11_Pin
                           |LED_12_Pin|LED_4_Pin|LED_5_Pin|LED_6_Pin
                           |LED_7_Pin|LED_8_Pin|LED_9_Pin|LED_10_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : SEG_0_Pin SEG_1_Pin SEG_2_Pin SEG_3_Pin
+                           SEG_4_Pin SEG_5_Pin SEG_6_Pin SEG_7_Pin
+                           SEG_8_Pin SEG_9_Pin SEG_10_Pin SEG_11_Pin
+                           SEG_12_Pin SEG_13_Pin */
+  GPIO_InitStruct.Pin = SEG_0_Pin|SEG_1_Pin|SEG_2_Pin|SEG_3_Pin
+                          |SEG_4_Pin|SEG_5_Pin|SEG_6_Pin|SEG_7_Pin
+                          |SEG_8_Pin|SEG_9_Pin|SEG_10_Pin|SEG_11_Pin
+                          |SEG_12_Pin|SEG_13_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LED_1_Pin LED_2_Pin LED_3_Pin LED_11_Pin
                            LED_12_Pin LED_4_Pin LED_5_Pin LED_6_Pin
